2195b5e14ad3e5a6125247be346bd0f7ea17a187
Kristoffer Sjogren <stoffe@gmail.com>
2016-01-23 14:26:27 +0100

    javadoc

--- core/src/main/java/org/deephacks/jobpipe/Pipeline.java
+++ core/src/main/java/org/deephacks/jobpipe/Pipeline.java
@@ -1,5 +1,14 @@
 package org.deephacks.jobpipe;
 
+/**
+ * A pipeline schedule generated at runtime.
+ */
 public interface Pipeline {
+
+  /**
+   * Called when the pipeline should execute, usually triggered from command line.
+   *
+   * @param context information of how the pipeline should be executed.
+   */
   void execute(PipelineContext context);
 }
